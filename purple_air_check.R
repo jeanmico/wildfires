eb56b6f4f896b52c3fb416929ddc7d4d5c3097d5
Jean Costello <costellojeanm@gmail.com>
2021-06-09 13:04:22 -0700

    filtering purpleair

--- purple_air_check.R
+++ purple_air_check.R
@@ -30,8 +30,11 @@ epa_pacorr <- ggplot(data = epa, aes(x= ymd(day_date), y=Daily.Mean.PM2.5.Concen
   geom_point(data = pa_corr, aes(x=ymd(day_date), y=pm25, color = measure_type, alpha=.2)) + 
   geom_line(data= epa_mean, aes(x = day_date, y = daymean)) +
   geom_line(data = epa_mean, aes(x = day_date, y = dayminus4sd)) +
+  geom_line(data = epa_mean, aes(x = day_date, y = daymin)) +
   theme_minimal()
 epa_pacorr
+
+ggsave(epa_pacorr, file = '~/wildfires/sdplot.png')
 
 pa_corr <- pa_corr %>% mutate(day_date = ymd(day_date))
 mean_pa <- merge(pa_corr, epa_mean, by = 'day_date')
@@ -39,3 +42,9 @@ mean_pa <- merge(pa_corr, epa_mean, by = 'day_date')
 # between Nov 8 and 21...which PA sensors are more than x standard deviations away from norm
 pa_remove <- mean_pa %>% filter(pm25 < dayminus4sd)
 pa_remove15 <- pa_remove %>% filter(day_date == ymd('2018-11-15'))
+
+lowpa <- pa_remove %>% filter(pm25 <20)
+View(lowpa)
+pa4788 <- pa_corr %>% filter(id == 4788)
+low_sensor <- ggplot(pa4788, aes(x = day_date, y = pm25)) + geom_point() + scale_x_date()
+low_sensor
