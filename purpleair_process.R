cabe58e783cdf5ecc13464d2776574ae2167ebf2
Jean Costello <costellojeanm@gmail.com>
2021-08-09 19:04:41 -0700

    save data for 2017-20

--- purpleair_process.R
+++ purpleair_process.R
@@ -1,4 +1,4 @@
-# this script is to read + process purpleair data
+# this script is to read, correct, and combine purpleair data into one file
 # data is housed on external drive due to size
 library(dplyr)
 fpath = '/Volumes/Padlock/purpleair'
@@ -24,7 +24,7 @@ for(i in 1:length(fnames)){
   
   sensid = strsplit(basename(fnames[i]), '\\.')[[1]][1]
   print(sensid)
-  outname = paste('/Volumes/Padlock/purpleair_2018_corr/', as.character(sensid), '.csv', sep = '')
+  outname = paste('/Volumes/Padlock/purpleair_all_corr/', as.character(sensid), '.csv', sep = '')
   if (!file.exists(outname)) {
   
     df <- read.csv(fnames[i])
@@ -39,51 +39,48 @@ for(i in 1:length(fnames)){
                 mean_pm25_1 = mean(pm25_1),
                 mean_temp = mean(temperature),
                 mean_humid = mean(humidity),
-                pm25_atm_corr = .52*mean_pm25_atm + .0024*mean_temp -.085*mean_humid + 5.71,
-                pm25_1_corr = .52*mean_pm25_1 + .0024*mean_temp -.085*mean_humid + 5.71)
-    print(colnames(df))
+                pm25_atm_corr = .52*mean_pm25_atm  -.085*mean_humid + 5.71,
+                pm25_1_corr = .52*mean_pm25_1  -.085*mean_humid + 5.71)
     print(nrow(df))
-    # get Jan 1 through dec 31 2018
-    nov18df <- df %>% filter(day_date <= as.Date('2018-12-31') & day_date >= as.Date('2018-01-01'))
 
-    # decide whether to write the file - are there at least 5 measurements in Nov 2018?
-    if(nrow(nov18df) >= 5) {
-      if (nrow(nov18df %>% filter(day_date <= as.Date('2018-11-30') & day_date >= as.Date('2018-11-01'))) >= 5) {
-        write.csv(nov18df, file = outname, row.names = FALSE)
-        purpledf = rbind(purpledf, nov18df)
-      }
+    # decide whether to write the file - are there at least 5 measurements?
+    if(nrow(df) >= 5) {
+      
+      write.csv(df, file = outname, row.names = FALSE)
+      purpledf = rbind(purpledf, df)
+      
     }
   }
 }
 
 sensor_loc = read.csv('~/GitHub/wildfires/bay_area_sensors.csv')
 
-fpath = '/Volumes/Padlock/purpleair_2018_corr'
+# fpath = '/Volumes/Padlock/purpleair_all_corr'
+# 
+# summ_cols = c("sensor_id","day_date","mean_pm25_atm","mean_pm25_1","mean_temp","mean_humid","pm25_atm_corr","pm25_1_corr")
+# 
+# fnames <- list.files(fpath, full.names = TRUE)
+# daydf = data.frame(matrix(ncol=length(summ_cols), nrow=0))
+# 
+# colnames(daydf) = summ_cols
+# myids = vector('list', 2998)
+# for (i in 1:length(fnames)) {
+#   tmp = read.csv(fnames[i])
+#   tmp_id = strsplit(basename(fnames[i]), '\\.')[[1]][1]
+#   tmp <- tmp %>% mutate(id = tmp_id)
+#   myids[[i]] <- tmp_id
+#   daydf <- rbind(daydf, tmp)
+# }
 
-summ_cols = c("sensor_id","day_date","mean_pm25_atm","mean_pm25_1","mean_temp","mean_humid","pm25_atm_corr","pm25_1_corr")
-
-fnames <- list.files(fpath, full.names = TRUE)
-daydf = data.frame(matrix(ncol=length(summ_cols), nrow=0))
-
-colnames(daydf) = summ_cols
-myids = vector('list', 2998)
-for (i in 1:length(fnames)) {
-  tmp = read.csv(fnames[i])
-  tmp_id = strsplit(basename(fnames[i]), '\\.')[[1]][1]
-  tmp <- tmp %>% mutate(id = tmp_id)
-  myids[[i]] <- tmp_id
-  daydf <- rbind(daydf, tmp)
-}
-
-# correction - fix
-daydf <- daydf %>% mutate(pm25_atm_corr = .524*mean_pm25_atm - 0.0852*mean_humid + 5.72,
-                          pm25_1_corr = .524*mean_pm25_1 - 0.0852*mean_humid + 5.72)
+# # correction - fix
+# daydf <- daydf %>% mutate(pm25_atm_corr = .524*mean_pm25_atm - 0.0852*mean_humid + 5.72,
+#                           pm25_1_corr = .524*mean_pm25_1 - 0.0852*mean_humid + 5.72)
 
 sensor_loc <- sensor_loc %>% dplyr::select(id, lat, lon, name, location_type)
 
-daydf = merge(sensor_loc, daydf, by = 'id') 
+daydf = merge(sensor_loc, purpledf, by.x = 'id', by.y = 'sensor_id') 
 
-write.csv(daydf, file='~/wildfires/2018_purpleair.csv', row.names = FALSE)
+write.csv(daydf, file='~/wildfires/all_purpleair.csv', row.names = FALSE)
 
 # filter to one day
 oneday = daydf %>% filter(as.Date(day_date) == as.Date('2018-11-15'))
